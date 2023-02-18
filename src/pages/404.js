25fd0163b3b46f7898136f1c1a7a05cdbdfebcfb
maxwell johnson <maxwellarrigona@gmail.com>
2023-02-18 09:59:49 -0700

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>Can you hear the tronbone... wa wa wa...</p>
+  </Layout>
+)
+
+export default NotFoundPage
